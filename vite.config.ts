883ecb9c7568efa5cbde3a5162195dbb2020aa93
mrKlm <clementmorel@free.fr>
2026-05-04 23:43:08 +0200

    Prepare le projet pour un deploiement Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const isElectron = process.env.ELECTRON === '1'
+const isNetlify = process.env.NETLIFY === 'true'
 
 export default defineConfig({
   plugins: [react()],
-  base: isElectron ? './' : '/vaso-shop/',
+  base: isElectron ? './' : isNetlify ? '/' : '/vaso-shop/',
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
